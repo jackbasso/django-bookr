6d31427f284ed227838205a64d05bcb06a6bc9bf
jackbasso <jackbasso.r@gmail.com>
2022-11-11 22:40:20 -0500

    database populated

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
   path('', views.welcome_view, name='welcome_view'),
+  path('books/', views.book_list, name='book_list'),
+  path('books/<int:id>/', views.book_detail, name='book_detail'),
+
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Book
+from .models import Book, Review
+from .utils import average_rating
+
 
 '''
 def welcome_view(request):
@@ -9,3 +11,25 @@ def welcome_view(request):
 '''
 def welcome_view(request):
     return render(request, 'base.html')
+
+def book_list(request):
+    books = Book.objects.all()
+    book_list = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews = 0
+        book_list.append({'book':book, 'book_rating':book_rating, 'number_of_reviews':number_of_reviews})
+    context = {
+        'book_list': book_list
+    }
+    return render(request, 'reviews/books_list.html', context)
+
+def book_detail(request, id):
+    book = get_object_or_404(Book, id=id)
+    reviews = book.review_set.all()
+    return render(request, 'reviews/book_detail.html', { 'book':book, 'reviews':reviews })
